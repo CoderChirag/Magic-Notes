53633fb503a985c19ea100208ab2114ea6e58bef
CoderChirag <chirag0174.be20@chitkara.edu.in>
2020-12-04 13:38:36 +0530

    Added Custom Notes Title, and gave some color styling

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 showNotes();
 
+// Adding a new note
 let addBtn = document.getElementById("add-btn");
 addBtn.addEventListener("click", function(e){
     let addTxt = document.getElementById("note-area");
+    let addTitle = document.getElementById("title");
     let notes = localStorage.getItem("notes")
     if (addTxt.value.trim() == ""){
         alert("Please Fill Up the Notes First");
+    }
+    else if (addTitle.value.trim() == "")  {
+        alert("Please Fill Up the Title First")
     }
     else{
         if (notes == null){
@@ -14,13 +19,15 @@ addBtn.addEventListener("click", function(e){
         else{
             notesObj = JSON.parse(notes);
         }
-        notesObj.push(addTxt.value);
+        notesObj.push([addTitle.value, addTxt.value]);
         localStorage.setItem("notes", JSON.stringify(notesObj));
         addTxt.value = "";
+        addTitle.value = "";
     }
     showNotes();
 });
 
+// Showing all the notes
 function showNotes(){
     let notes = localStorage.getItem("notes");
     if (notes == null){
@@ -33,9 +40,9 @@ function showNotes(){
     notesObj.forEach(function(element, index){
         html += `
                 <div class="notes-div">
-                <h1 class="notes-head">Note ${index + 1}</h1>
+                <h1 class="notes-head">${element[0]}</h1>
                 <div class="notes-content">
-                    <p>${element}</p>
+                    <p>${element[1]}</p>
                     <button id="${index}" onclick="deleteNote(this.id)" class="btn">Delete Note</button>
                 </div>
                 </div>`
@@ -49,6 +56,7 @@ function showNotes(){
     }
 }
 
+// Deleting a Note
 function deleteNote(index){
     let notes = localStorage.getItem("notes");
     if (notes == null){
@@ -63,6 +71,7 @@ function deleteNote(index){
     showNotes();
 }
 
+// Searching for a note
 let search = document.getElementById("searchTxt");
 search.addEventListener("click", function(){
     console.log(search.value);
@@ -76,7 +85,8 @@ search.addEventListener("input", function(){
         console.log("we are here");
         Array.from(noteCards).forEach(function(element){
             let cardTxt = element.getElementsByTagName("p")[0].innerText.toLowerCase();
-            if (cardTxt.includes(inputVal)){
+            let cardTitle = element.getElementsByTagName("h1")[0].innerText.toLowerCase();
+            if (cardTxt.includes(inputVal) || cardTitle.includes(inputVal)){
                 element.style.display = "inline-block";
             }
             else{
